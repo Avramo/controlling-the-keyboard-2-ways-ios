4a06270e8cd2fa59f3364fa4e557e2a11b4dbbe9
Avraham Kirsch <etrog95@gmail.com>
2020-09-29 14:31:34 +0300

    first commit

--- Controlling the keyboard - 2 ways/ViewController.swift
+++ Controlling the keyboard - 2 ways/ViewController.swift
@@ -8,13 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true) // closes keyboard when touch on screen
+    }
+    
+    // what to do upon keyboard 'enter'
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
 
 }
 
